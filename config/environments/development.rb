092e1eab9ec7f8c6246164f2a8e96d9df2f7ba04
Mike Nicklas <mike.nicklas@gmail.com>
2018-10-23 08:04:09 -0400

    Share sessions across subdomains

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  config.session_store :cookie_store, key: '_featureloop_beta_session', domain: "lvh.me"
 end
