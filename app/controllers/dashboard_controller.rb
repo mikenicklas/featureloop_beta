2debf77d0c3d8cda6ddf284347a2c70187605c27
Mike Nicklas <mike.nicklas@gmail.com>
2018-10-28 12:13:05 -0400

    Automatically redirect to subdomain instead of showing app dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@ class DashboardController < ApplicationController
 
   def index
     redirect_to new_product_path unless current_user.owns_product?
+    # TODO: is this long term viable?
+    redirect_to root_url(subdomain: current_user.product.subdomain)
   end
 
 end
